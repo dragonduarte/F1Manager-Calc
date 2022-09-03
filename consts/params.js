b0b22e379aa0194e7c13d6333a49853a3005c836
dragonduarte <66562458+dragonduarte@users.noreply.github.com>
2022-09-03 14:43:30 +0100

    Translation PT-BR

--- consts/params.js
+++ consts/params.js
@@ -1,8 +1,8 @@
 
 export const CarSetupParams = [
   {
-    // name: "Front Wing Angle",
-    name: "Front Angle",
+    // name: "Ângulo de Asa Dianteira",
+    name: "Ângulo Dianteiro",
     index: 0,
     min: 0,
     max: 10,
@@ -12,8 +12,8 @@ export const CarSetupParams = [
     render: x => x.toFixed(1),
   },
   {
-    // name: "Rear Wing Angle",
-    name: "Rear Angle",
+    // name: "Ângulo da Asa Traseira",
+    name: "Ângulo Traseiro",
     index: 1,
     min: 9,
     max: 16,
@@ -23,7 +23,7 @@ export const CarSetupParams = [
     render: x => x.toFixed(1),
   },
   {
-    name: "Anti-Roll",
+    name: "Distribuição Antirrolagem",
     index: 2,
     min: 1,
     max: 9,
@@ -38,7 +38,7 @@ export const CarSetupParams = [
     },
   },
   {
-    name: "Tyre Camber",
+    name: "Cambagem dos Pneus",
     index: 3,
     min: 2.7,
     max: 3.5,
@@ -48,7 +48,7 @@ export const CarSetupParams = [
     render: x => `-${x.toFixed(2)}°`,
   },
   {
-    name: "Toe-Out",
+    name: "Ângulo",
     index: 4,
     min: 0,
     max: 1,
@@ -62,36 +62,36 @@ export const CarSetupParams = [
 
 export const BiasParams = [
   {
-    name: "Oversteer",
+    name: "Sair de Traseira",
     index: 0,
     offset: 0.5,
     effect: [0.4, -0.4, -0.1, 0.1, 0],
     render: x => x.toFixed(3),
   },
   {
-    // name: "Braking Stability",
-    name: "Braking",
+    // name: "Estabilidade de frenagem",
+    name: "Frenagem",
     index: 1,
     offset: 0.45,
     effect: [-0.2, 0.2, 0.15, -0.25, 0.2],
     render: x => x.toFixed(3),
   },
   {
-    name: "Cornering",
+    name: "Curvas",
     index: 2,
     offset: 0.2,
     effect: [0.3, 0.25, -0.15, 0.25, -0.05],
     render: x => x.toFixed(3),
   },
   {
-    name: "Traction",
+    name: "Tração",
     index: 3,
     offset: 0.25,
     effect: [-0.15, 0.25, 0.5, -0.1, 0],
     render: x => x.toFixed(3),
   },
   {
-    name: "Straights",
+    name: "Retas",
     index: 4,
     offset: 1,
     effect: [-0.1, -0.9, 0, 0, 0],
